3312acd34abff5e36b68beb08e760ac89d69f131
Rackon13 <tatow2001@gmail.com>
2022-07-26 18:34:45 -0300

    Creating User Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
+import email
+from statistics import mode
 from django.db import models
-
-# Create your models here.
+class User(models.Model):
+    nome = models.CharField(max_length=40)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=20)
